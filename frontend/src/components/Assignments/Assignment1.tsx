7059f57980772753e013765ba2966afee84f1322
Thomas <denhollander.thomas@gmail.com>
2022-05-26 17:53:40 +0200

    Assignment2

---
+++ frontend/src/components/Assignments/Assignment1.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { AccountInfoDisplay } from "../AccountInfo";
+
+export const Assignment1 = () => (
+  <div>
+    <div>
+      <p>
+        {" "}
+        Create a new React component to show the account name and ETH balance of
+        the connected wallet.{" "}
+      </p>
+    </div>
+    <AccountInfoDisplay />
+  </div>
+);
